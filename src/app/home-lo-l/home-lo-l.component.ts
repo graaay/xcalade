bcd7ebf95499827a0ae8b66f3bf9fa163137a630
graaay <abneralecs@gmail.com>
2019-11-29 21:19:36 -0300

    imagem setada no lol, tabela de top players e inicio de tabela de apostas

--- src/app/home-lo-l/home-lo-l.component.ts
+++ src/app/home-lo-l/home-lo-l.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeLoLComponent implements OnInit {
 
+  users = [
+    {pos: 1, name: "Abner", points: 20},
+    {pos: 2, name: "Eduardo", points: 18},
+    {pos: 3, name: "Mateus", points: 15},
+    {pos: 1, name: "Felipe", points: 14},
+    {pos: 2, name: "Lucas", points: 13},
+    {pos: 3, name: "Guilherme", points: 10},
+    {pos: 1, name: "Bruno", points: 9},
+    {pos: 2, name: "Douglas", points: 8},
+    {pos: 3, name: "Kaabulozo", points: 7}
+  ]
+
+  rounds = [
+    {teamA: "KBM", teamB: "PNG"},
+    {teamA: "PRG", teamB: "ITZ"},
+    {teamA: "KDS", teamB: "RED"},
+    {teamA: "RDP", teamB: "CNB"},
+  ]
   constructor() { }
 
   ngOnInit() {
